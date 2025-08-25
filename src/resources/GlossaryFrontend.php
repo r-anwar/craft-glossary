658f327ba9044e404088f4dcf06d8097ff81fa42
Anwar Reza <reza.anwar@wien.gv.at>
2025-08-25 08:03:31 +0200

    switched from tippyjs tooltips to native popover functionality
    added a css fallback in case browser doesnt support popover

--- src/resources/GlossaryFrontend.php
+++ src/resources/GlossaryFrontend.php
@@ -14,12 +14,14 @@ class GlossaryFrontend extends AssetBundle
         $this->sourcePath = '@codemonauts/glossary/resources';
 
         $this->js = [
-            'js/Glossary.js',
+            /*'js/Glossary.js',*/
+            'js/Popover-Hover.js'
         ];
 
         $this->css = [
-            'css/tippy.css',
+            /*'css/tippy.css',*/
             'css/light.css',
+            'css/popover.css'
         ];
 
         parent::init();
